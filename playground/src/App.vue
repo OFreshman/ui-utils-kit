e828e6eba78bec4c2d82fef84a35a7b1dd37d159
Ethan <415561402@qq.com>
2025-02-20 14:08:23 +0800

    docs: 完善 captureElementAsImage 测试

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,24 +1,11 @@
 <template>
   <div>
-    <button @click="onPrint">打印</button>
-    <div class="print">
-      <ul ref="printEl">
-        <li v-for="i in 20" :key="i">{{ i }}</li>
-      </ul>
-    </div>
+    <html2canvas></html2canvas>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { captureElementAsImage } from "ui-utils-kit";
-import { ref } from "vue";
-
-const printEl = ref<HTMLElement | null>(null);
-
-async function onPrint() {
-  const res = await captureElementAsImage(printEl.value);
-  console.log("res: ", res);
-}
+import html2canvas from "./components/html2canvas.vue";
 </script>
 
 <style>
@@ -27,19 +14,5 @@ html, body {
   height: 100%;
   padding: 0;
   margin: 0;
-}
-.print {
-  width: 300px;
-  overflow: auto;
-  ul {
-    width: 100%;
-    list-style: none;
-  }
-  ul li {
-    width: 100%;
-    height: 20px;
-    line-height: 20px;
-    text-align: center;
-  }
 }
 </style>
---
+++ playground/src/components/html2canvas.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <hr />
+    <p>html2canvas-pro test</p>
+    <button @click="onPrint">打印</button>
+    <div class="print">
+      <ul ref="printEl">
+        <li v-for="i in 20" :key="i">{{ i }}</li>
+      </ul>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { captureElementAsImage } from "ui-utils-kit";
+import { ref } from "vue";
+
+const printEl = ref<HTMLElement | null>(null);
+
+async function onPrint() {
+  const res = await captureElementAsImage(printEl.value);
+  console.log("res: ", res);
+}
+</script>
+
+<style scoped lang="scss">
+.print {
+  width: 300px;
+  height: 150px;
+  overflow: auto;
+  ul {
+    padding: 5px;
+    width: 100%;
+    list-style: none;
+  }
+  ul li {
+    width: 100%;
+    height: 20px;
+    line-height: 20px;
+    border: 1px solid #aaa;
+    margin: 5px 0;
+    text-align: center;
+  }
+}
+</style>
